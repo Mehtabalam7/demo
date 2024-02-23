37657571464e17994feec982ffb3fc0ab3d863e8
Mehtabalam7 <116348083+Mehtabalam7@users.noreply.github.com>
2024-02-23 19:25:42 +0530

    Add files via upload

---
+++ Asign.java
@@ -0,0 +1,23 @@
+public class Asign{
+    public static void main(String[] args) {
+        int x[]={12,20,015};
+        for(int i=0;i<x.length;i++)
+        {
+            System.out.println(x[i] + " ");
+        }
+        boolean k = true;
+        boolean y = false;
+        boolean z=(k==y);
+        System.out.println(z);
+
+        int j = 5;
+        j *= (2+8);
+        System.out.println(j);
+
+        byte l = 127;
+        l++;
+        ++l;
+        System.out.println(l);
+
+    }
+}
